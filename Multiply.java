eb8b03916013f042c66d78db767ed9f55cc42b40
Kishan Kulkarni <kulkarnikishan610@gmail.com>
2022-01-26 16:58:33 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,7 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 2;
+        int b =3 ;
+        System.out.println("The product of 2 numbers is" + a*b);
+    }
+}
