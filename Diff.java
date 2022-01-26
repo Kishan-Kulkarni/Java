f417ddee8514af02e6847258699b3074d6919f6b
Kishan Kulkarni <kulkarnikishan610@gmail.com>
2022-01-26 16:42:15 +0530

    adding Diff.java

---
+++ Diff.java
@@ -0,0 +1,8 @@
+public class Diff {
+    public static void main(String[] args) {
+        int a = 45 ;
+        int b = 30;
+        int diff = a - b;
+        System.out.println("The difference is " + diff);
+    }
+}
